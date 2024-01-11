acfdf4591a1d52215c3e578300d56859cec12c26
amandatsu <121841909+amandatsu@users.noreply.github.com>
2024-01-11 16:11:33 -0600

    Update covid_agg1.py
    Added import statements

---
+++ covid_agg1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+time = pd.read_csv('dataset/Time.csv')
+timeage = pd.read_csv('dataset/TimeAge.csv')
+timegender = pd.read_csv('dataset/TimeGender.csv')
+timeprovince = pd.read_csv('dataset/TimeProvince.csv')
+
